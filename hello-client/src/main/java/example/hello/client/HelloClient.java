27f4284496002b1c30ca722b5a87279f84b22284
gregwhitaker <gwhitake@gmail.com>
2020-01-17 07:13:29 -0800

    Initial Commit

---
+++ hello-client/src/main/java/example/hello/client/HelloClient.java
@@ -0,0 +1,12 @@
+package example.hello.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloClient {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloClient.class);
+
+    public static void main(String... args) {
+        
+    }
+}
---
+++ hello-service/src/main/java/example/hello/service/HelloService.java
@@ -0,0 +1,12 @@
+package example.hello.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloService {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloService.class);
+
+    public static void main(String... args) throws Exception {
+
+    }
+}
